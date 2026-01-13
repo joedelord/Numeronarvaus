f3553273f59a32c8ee6402dc5202d18516473152
Mika Isomäki <mika.j.isomaki@gmail.com>
2026-01-13 16:43:13 +0200

    fix bugs

--- README.py
+++ README.py
@@ -11,19 +11,38 @@ import random
 class NumberGuessingGame:
 
     def __init__(self):
-        pass
+        self.count = 0
+        self.generate_random_number()
        
     def generate_random_number(self):
         self.number = random.randint(1, 10)
       
     def ask_number(self):
-        pass 
-              
+        while True:
+            try:
+                number = int(input("Arvaa numero väliltä 1-10: "))
+            except:
+                continue
+            if number < 1:
+                print("Numero oli liian pieni.")
+                continue
+            elif number > 10:
+                print("Numero on liian suuri.")
+                continue
+            else:
+                break
+        return number              
+
     def add_guess_count(self):
         self.count += 1
            
     def main_game_loop(self):
-        pass
+        while True:
+            number_user = self.ask_number()
+            self.add_guess_count()
+            if number_user == self.number:
+                print(f"Oikein. Arvasit {self.count} kertaa.")
+                break
 
 game = NumberGuessingGame()
 
